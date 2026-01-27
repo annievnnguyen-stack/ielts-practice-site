1cbb1591406050eea25355c1cc72b5e568f94ccf
anh nguyen <anh.nguyen@globalagilitysolutions.com>
2026-01-27 14:06:26 +0700

    Add Back to Home button on all pages

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,24 @@
 import Link from "next/link";
+import styles from "./Nav.module.css";
+
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <Link href="/" className={styles.home}>
+        ⬅ Back to Home
+      </Link>
+
+      <Link href="/listening">🎧 Listening</Link>
+      <Link href="/reading">📘 Reading</Link>
+      <Link href="/speaking">🗣 Speaking</Link>
+      <Link href="/writing">✍️ Writing</Link>
+    </nav>
+  );
+}
+
+
+
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Nav() {
