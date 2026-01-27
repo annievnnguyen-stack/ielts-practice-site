128874351c8c2a74ecac167919d80d068a85c4fb
anh nguyen <anh.nguyen@globalagilitysolutions.com>
2026-01-27 11:19:32 +0700

    Add full Next.js project structure

---
+++ pages/speaking.js
@@ -0,0 +1,8 @@
+export default function Speaking() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>IELTS Speaking</h1>
+      <p>Describe a book you recently read.</p>
+    </div>
+  );
+}
