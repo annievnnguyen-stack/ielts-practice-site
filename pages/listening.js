ebd8d0e0ecdba121afe02b07ea0f5dbdbc7b2607
anh nguyen <anh.nguyen@globalagilitysolutions.com>
2026-01-30 09:01:43 +0700

    Add Back to Home button on all pages

--- pages/listening.js
+++ pages/listening.js
@@ -2,9 +2,8 @@ import ExamLayout from "../components/ExamLayout";
 
 export default function Listening() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>IELTS Listening</h1>
+    <ExamLayout title="IELTS Listening" timeInMinutes={30}>
       <p>Audio-based practice will be added here.</p>
-    </div>
+    </ExamLayout>
   );
 }
