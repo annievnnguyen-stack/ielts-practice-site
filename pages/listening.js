87ec0a312bf5da2bef22ddae21a5090e7447a237
anh nguyen <anh.nguyen@globalagilitysolutions.com>
2026-02-10 11:14:02 +0700

    Update listening page

--- pages/listening.js
+++ pages/listening.js
@@ -5,9 +5,19 @@ export default function Listening() {
     <ExamLayout title="IELTS Listening" timeInMinutes={30}>
       
       <p><strong>Instructions</strong></p>
-      <p>
-        You will hear a telephone conversation between a father and his daughter Hannah about renting a flat. You will see that there is an example that has been done for you. Type the appropriate <strong>ANSWERS A or B or C in the box </strong>.
+
+      <p>You will hear a telephone conversation between a father and his daughter Hannah about renting a flat. </p>
+
+      <p style={{ marginTop: 20 }}>
+        <em>You will hear the recording ONCE only.</em>
       </p>
+
+      <p style={{ marginTop: 20 }}>
+      <p>You will see that there is an example that has been done for you. Type the appropriate <strong>ANSWERS A or B or C in the box </strong>.
+      </p>
+
+
+
 
       {/* Audio */}
       <audio controls style={{ width: "100%", margin: "20px 0" }}>
@@ -15,44 +25,57 @@ export default function Listening() {
         Your browser does not support the audio element.
       </audio>
 
+
+
       {/* Questions */}
-      <h3>Library Membership Form</h3>
+
+
+      <h3>ANSWERS</h3>
+     <p>Now listen and answer questions 1 to 5. </p>
 
       <table style={{ width: "100%", borderCollapse: "collapse" }}>
         <tbody>
           <tr>
-            <td>Name:</td>
+            <td>Example:</td>
             <td>
-              <input type="text" placeholder="1 _______" />
+              <input type="text" placeholder="0 B" />
+            </td>
+          <tr>
+            <td>Question 1:</td>
+            <td>
+              <input type="text" placeholder="1 _" />
             </td>
           </tr>
 
           <tr>
-            <td>Student ID:</td>
+            <td>Question 2:</td>
             <td>
-              <input type="text" placeholder="2 _______" />
+              <input type="text" placeholder="2 _" />
             </td>
           </tr>
 
           <tr>
-            <td>Course:</td>
+            <td>Question 3:</td>
             <td>
-              <input type="text" placeholder="3 _______" />
+              <input type="text" placeholder="3 _" />
             </td>
           </tr>
 
           <tr>
-            <td>Library card valid for:</td>
+            <td>Question 4:</td>
             <td>
-              <input type="text" placeholder="4 _______ months" />
+              <input type="text" placeholder="4 _" />
+            </td>
+          <tr>
+            <td>Question 5:</td>
+            <td>
+              <input type="text" placeholder="5 _" />
             </td>
           </tr>
         </tbody>
       </table>
 
-      <p style={{ marginTop: 20 }}>
-        <em>You will hear the recording ONCE only.</em>
-      </p>
+
 
     </ExamLayout>
   );
